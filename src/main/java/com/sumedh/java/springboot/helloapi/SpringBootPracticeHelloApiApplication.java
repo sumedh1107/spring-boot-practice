a7c5bf3f4c3ea93d7519c72ca2eadb5d91141eaf
sumedh1107 <sumeghd@gmail.com>
2026-01-11 16:36:19 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/sumedh/java/springboot/helloapi/SpringBootPracticeHelloApiApplication.java
@@ -0,0 +1,13 @@
+package com.sumedh.java.springboot.helloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeHelloApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeHelloApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sumedh/java/springboot/helloapi/SpringBootPracticeHelloApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumedh.java.springboot.helloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPracticeHelloApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
